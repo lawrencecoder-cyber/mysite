1e155b178ba2e542e0667b1506824a709c2adb5d
Lawrence Coder <lawrencecoder@gmail.com>
2026-03-21 01:43:49 +0300

    django blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$nbk)4r=x&)$nxc%hf2^9ol8b*5-j0a84m=x*sa-0!4e=idcp@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mysite.onrender.com', 'www.mysite.onrender.com']
 
 
 # Application definition
