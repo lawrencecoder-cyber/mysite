7da7e60c5125c629ac50d51c79f1cb3ced68d24f
Lawrence Coder <lawrencecoder@gmail.com>
2026-03-29 23:34:50 +0300

    new changes

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    featured_image = models.ImageField(upload_to='posts/', null=True, blank=True)
+    is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
+# Create your views here.from .models import Post
+
 from .models import Post
 
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(is_published=True).order_by('-created_at')
     return render(request, 'home.html', {'posts': posts})
