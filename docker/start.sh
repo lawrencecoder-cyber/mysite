8a7f7b5e85221f5036a4690e9bf892274e28afea
Lawrence Coder <lawrencecoder@gmail.com>
2026-05-03 14:26:31 +0300

    update

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@ echo "Starting service: $SERVICE_TYPE"
 
 if [ "$SERVICE_TYPE" = "web" ]; then
     python manage.py migrate
-    gunicorn app.wsgi:application --bind 0.0.0.0:$PORT
+    gunicorn mysite.wsgi:application --bind 0.0.0.0:$PORT
 
 elif [ "$SERVICE_TYPE" = "worker" ]; then
     celery -A app worker --loglevel=info
