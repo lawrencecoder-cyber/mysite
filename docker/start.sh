8d594d8869da3a6f586c61109c6a83b9996bbd98
Lawrence Coder <lawrencecoder@gmail.com>
2026-05-03 11:23:53 +0300

    New changes

---
+++ docker/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting service: $SERVICE_TYPE"
+
+if [ "$SERVICE_TYPE" = "web" ]; then
+    python manage.py migrate
+    gunicorn app.wsgi:application --bind 0.0.0.0:$PORT
+
+elif [ "$SERVICE_TYPE" = "worker" ]; then
+    celery -A app worker --loglevel=info
+
+elif [ "$SERVICE_TYPE" = "beat" ]; then
+    celery -A app beat --loglevel=info
+
+else
+    echo "Unknown SERVICE_TYPE"
+fi
